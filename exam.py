a1c9b5dbf8b982e358357d4beb338f6fc1dcf6b1
bharat <ashwinimurugan08072006@gmail>
2024-05-17 04:21:19 -0700

    new file

---
+++ exam.py
@@ -0,0 +1,14 @@
+class bank :
+    def __init__(self,name,account_no):
+        self.name=name
+        self.account_no=account_no
+    def add_bineficiary(self,name=None,account_no=None):
+        if name:
+            self.name=name
+        if account_no:
+            self.account_no=account_no
+    def manage_binificiary(self,account_no):
+        if account_no=account_no:
+            self.account_no=account_no
+            
+            
